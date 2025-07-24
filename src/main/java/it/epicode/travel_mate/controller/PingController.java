e18d7c924089d773effcb0660a7df86fcecb2f54
Roberto Albergo <ralbergo7@gmail.com>
2025-07-24 09:44:43 +0200

    Create PingController.java

---
+++ src/main/java/it/epicode/travel_mate/controller/PingController.java
@@ -0,0 +1,13 @@
+package it.epicode.travel_mate.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
