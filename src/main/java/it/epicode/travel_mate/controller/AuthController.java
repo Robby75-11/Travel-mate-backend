2654366a1694830ecabeb502d16929b47eec3111
Roberto Albergo <ralbergo7@gmail.com>
2025-07-24 11:08:39 +0200

    Update AuthController.java

--- src/main/java/it/epicode/travel_mate/controller/AuthController.java
+++ src/main/java/it/epicode/travel_mate/controller/AuthController.java
@@ -14,7 +14,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "*")
 public class AuthController {
 
     @Autowired
