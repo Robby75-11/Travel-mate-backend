f52a7b3f2879d54f23a4ad243d356c8eaafb3de1
Roberto Albergo <ralbergo7@gmail.com>
2025-07-24 09:32:17 +0200

    Forza component scan su it.epicode.travel_mate

--- src/main/java/it/epicode/travel_mate/TravelMateApplication.java
+++ src/main/java/it/epicode/travel_mate/TravelMateApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "it.epicode.travel_mate")
 @EnableAsync
 public class
 TravelMateApplication {
