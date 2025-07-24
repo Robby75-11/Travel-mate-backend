0b1d49111477c88f117c738dd4b98cbe33daecd0
Roberto Albergo <ralbergo7@gmail.com>
2025-07-24 13:13:29 +0200

    Update UtenteResponseDto.java

--- src/main/java/it/epicode/travel_mate/dto/UtenteResponseDto.java
+++ src/main/java/it/epicode/travel_mate/dto/UtenteResponseDto.java
@@ -3,9 +3,11 @@ package it.epicode.travel_mate.dto;
 import it.epicode.travel_mate.enumeration.Ruolo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UtenteResponseDto {
 
     private Long id;
