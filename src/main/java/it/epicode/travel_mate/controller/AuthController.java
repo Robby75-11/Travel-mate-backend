056ac53a196927d8b3e1c8d40d750f267518ef93
Roberto Albergo <ralbergo7@gmail.com>
2025-08-17 10:33:16 +0200

    Update AuthController.java

--- src/main/java/it/epicode/travel_mate/controller/AuthController.java
+++ src/main/java/it/epicode/travel_mate/controller/AuthController.java
@@ -12,7 +12,6 @@ import it.epicode.travel_mate.service.UtenteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
